0081bb01bed38e362f760cf58dbd7a9d4df280f4
task11 <task11@naver.com>
2021-12-06 21:09:57 +0900

    Add OAuthForm

---
+++ src/components/OAuthForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { GoogleAuthProvider } from "firebase/auth";
+
+
+const OAuthForm = () => {
+  const provider = new GoogleAuthProvider();
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default OAuthForm;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,13 @@
 import AuthForm from "components/AuthForm";
+import OAuthForm from "components/OAuthForm";
 import React from "react";
 
 const Auth = () => {
   return (
-    <AuthForm />
+    <>
+      <AuthForm />
+      <OAuthForm />
+    </>
   );
 };
 
