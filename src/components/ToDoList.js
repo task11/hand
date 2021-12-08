0c6dd890133c787bca31dafed6e0fed98112b2b7
task11 <task11@naver.com>
2021-12-08 21:50:12 +0900

    Add ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { dbService } from "fBase";
-import { addDoc, collection, onSnapshot, orderBy, query, where } from "@firebase/firestore";
+import { authService, dbService } from "fBase";
+import { addDoc, collection, doc, onSnapshot, orderBy, query, updateDoc, where } from "@firebase/firestore";
 
 
 const ToDoList = ({ userObj }) => {
 
   const [toDo, setToDo] = useState("");
   const [error, setError] = useState("");
-  const [checked, setChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(true); // check box value 초기 값이 true여야지 첫 번쨰 클릭때 onCheck 함수에서 true로 바뀜
   const [toDos, setToDos] = useState([]);
 
   const onChange = (event) => {
@@ -35,13 +35,17 @@ const ToDoList = ({ userObj }) => {
   }
 
   const onCheck = (prev) => {
-    setChecked((prev) => !prev)
-    console.log("클릭");
+    setIsChecked((prev) => !prev);
     // 데이터베이스 업데이트 코드 추가
+    const washingtonRef = doc(dbService, "todos", prev.target.name);
+
+    updateDoc(washingtonRef, {
+      done: isChecked
+    });
   };
 
   useEffect(() => {
-    const q = query(collection(dbService, "todos"), orderBy("createdAt")); //, where("creatorId", "==", userObj.uid)
+    const q = query(collection(dbService, "todos"), orderBy("createdAt"), where("creatorId", "==", authService.currentUser.uid)); //userObj.uid가 왜 안될까 ?
 
     // unsubscribe(); 유저 로그 아웃 시에 onSnapshot 수신 대기 상태 제거해줘야함
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -53,7 +57,10 @@ const ToDoList = ({ userObj }) => {
       });
       setToDos(toDoArray);
     });
+
   }, [])
+
+
 
   return (
     <div>
@@ -76,7 +83,12 @@ const ToDoList = ({ userObj }) => {
           {toDos.map((toDo) => {
             return (
               <div key={toDo.id}>
-                <input type="checkbox" value={checked} onChange={onCheck} />
+                <input type="checkbox"
+                  value={isChecked}
+                  onChange={onCheck}
+                  checked={toDo.done}
+                  name={toDo.id}
+                />
                 <li>{toDo.task}</li>
               </div>
             );
