a18da1de8aad67ce5bb2defaaced58e5aa810a7a
task11 <task11@naver.com>
2021-12-11 13:48:11 +0900

    Add fullCalendarAPI

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 const Calendar = ({ userObj }) => {
   console.log(userObj);
 
   return (
-    <span>Calendar</span>
+    <div className="App">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+      />
+    </div>
 
   );
 
