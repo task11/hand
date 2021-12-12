8a65b19a44b46760a233f727a7a5d0b07cd899ec
task11 <task11@naver.com>
2021-12-12 16:27:23 +0900

    Install react-modal

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,8 @@ function App() {
       }
       setInit(true);
     });
-  }, [])
+  }, []);
+
 
   return (
     <div>
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,31 +1,89 @@
-import React from "react";
-import FullCalendar from '@fullcalendar/react';
+import React, { useState } from "react";
+import FullCalendar, { formatDate } from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import Modal from "react-modal";
 
 const Calendar = ({ userObj }) => {
+  const [eventColor, SetEventColor] = useState("red");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen((prev) => !prev); s
+  }
+
   console.log(userObj);
 
   const handleDateClick = (arg) => { // bind with an arrow function calendar 클릭 이벤트
     alert(arg.dateStr)
   }
 
-  const renderEventContent = (event) => {
-    console.log(event.timeText);
+  let str = formatDate(new Date(), {
+    month: 'long',
+    year: 'numeric',
+    day: 'numeric'
+  });
+  console.log(str);// today
+
+  const events = [
+    {
+      id: 1,
+      title: 'event 1',
+      start: '2021-12-14T10:00:00',
+      end: '2021-12-14T12:00:00',
+    },
+    {
+      id: 2,
+      title: 'event 2',
+      start: '2021-12-16T13:00:00',
+      end: '2021-12-16T18:00:00',
+    },
+    { id: 3, title: 'event 3', start: '2021-12-17', end: '2021-12-20' },
+  ];
+
+  const eventClick = (e) => {
+    console.log(e.event.id)
   }
 
+  const newButtonClick = () => {
+    console.log('new event')
+  }
+
+
+
+
+
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      dateClick={handleDateClick} // 클릭 이벤트
-      eventContent={renderEventContent} //
-      events={[
-        { title: 'event 1', date: '2021-12-05' },
-        { title: 'event 2', date: '2021-12-12' }
-      ]}
-    />
+    <>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          center: 'dayGridMonth,timeGridWeek,timeGridDay new',
+        }}
+        dateClick={handleDateClick} // 클릭 이벤트
+        customButtons={{
+          new: {
+            text: 'new',
+            click: newButtonClick,
+          },
+        }}
+        eventClick={eventClick}
+        events={events}
+        eventColor={eventColor}
+      />
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={toggleModal}
+        contentLabel="My dialog"
+      >
+        <div>My Modal</div>
+        <button onClick={toggleModal}>Close</button>
+      </Modal>
+    </>
+
+
   );
 
 }
