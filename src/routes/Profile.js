008edc41d00fba6c3f51ec0ae12f7a5582f0b9b8
task11 <task11@naver.com>
2021-12-14 23:27:25 +0900

    Add Profile Edit View

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,61 @@
-import React from "react";
+import { updateProfile } from "firebase/auth";
+import React, { useState } from "react";
 
 const Profile = ({ userObj }) => {
+  const [newPhoto, setNewPhoto] = useState(userObj.photoURL);
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
 
+
+  const changeProfileImg = () => {
+    console.log("change");
+  }
+
+  const deleteProfileImg = () => {
+    console.log("delete");
+  }
+  const onChange = (event) => {
+    const { target: { value } } = event;
+    setNewDisplayName(value);
+  }
+
+  const onSubmitProfileInfo = (event) => {
+    event.preventDefault();
+
+  }
 
   return (
-    <div>
+    <>
       <h2>프로필 편집</h2>
-      <img src={userObj.photoURL} alt={userObj.displayName} />
-      <span>Hi {userObj.displayName}</span>
-    </div>
+      <form onSubmit={onSubmitProfileInfo}>
+        <div style={{ display: "flex" }}>
+          <div>
+            <img style={{ borderRadius: "40px" }} src={newPhoto} alt={userObj.displayName} />
+            <br />
+            <button
+              name="changeImg"
+              onClick={changeProfileImg}
+            >이미지 변경</button>
+            <button
+              name="deleteImg"
+              onClick={deleteProfileImg}
+            >이미지 삭제</button>
+          </div>
+          <div>
+            <h1>이름</h1>
+            <input
+              type="text"
+              name="changeName"
+              value={newDisplayName}
+              onChange={onChange}
+            ></input>
+          </div>
+        </div>
+        <div>
+          <button type="submit">완료</button>
+          <button type="reset">취소</button>
+        </div>
+      </form>
+    </>
   );
 };
 
--- src/routes/Setting.js
+++ src/routes/Setting.js
@@ -2,6 +2,7 @@ import Preference from "components/Preference";
 import React from "react";
 
 const Setting = ({ userObj }) => {
+  console.log(userObj);
   return (
     <div>
       <div>
