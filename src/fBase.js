f18abf622d7c2976e808558dd0543eba63a76526
task11 <task11@naver.com>
2022-06-06 13:53:42 +0900

    chore: dotenv 적용 및 gitignore 추가

--- src/fBase.js
+++ src/fBase.js
@@ -1,43 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiOglV0XyLymISxrqbjLlovt_8Yz2VDE8",
-  authDomain: "hand-f5ddb.firebaseapp.com",
-  projectId: "hand-f5ddb",
-  storageBucket: "hand-f5ddb.appspot.com",
-  messagingSenderId: "597052928227",
-  appId: "1:597052928227:web:2880367f95cf5e400e15d6",
-  measurementId: "G-E4D0H8W763"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
 
+export const storageService = getStorage();
 export const dbService = getFirestore();
 export const authService = getAuth();
-export const storageService = getStorage();
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENTID
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const authService = getAuth();
-
-// export default authService;
